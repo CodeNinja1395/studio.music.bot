7d350718ceb415f0b1922344aedc5425b555eafb
Gregory Kisenko <heyapple1395@gmail.com>
2021-03-17 00:14:48 +0200

    config updated

--- index.js
+++ index.js
@@ -2,98 +2,98 @@ process.env.NTBA_FIX_319 = 1
 const TelegramBot = require('node-telegram-bot-api')
 
 const { startOptions } = require('./lib/keyboardOptions')
-const { startMessage, dateRegexp, durationRegexp, token } = require('./lib/config')
+const { startMessage, dateRegexp, durationRegexp, token } = require('./config')
 const {
-    chooseTime,
-    onSelectTime,
-    onNewEvent,
-    addBooking,
-    goHome,
-    myBookings,
-    cancelBooking,
-    deleteBooking
+  chooseTime,
+  onSelectTime,
+  onNewEvent,
+  addBooking,
+  goHome,
+  myBookings,
+  cancelBooking,
+  deleteBooking
 } = require('./src/callbackQueries')
 const { ScheduleManager } = require('./lib/ScheduleManager')
 
 const manager = new ScheduleManager()
 
 const bot = new TelegramBot(token, {
-    polling: true
+  polling: true
 })
 
 bot.on('message', async (msg) => {
-    console.log(msg)
+  console.log(msg)
 
-    if (msg.text === '/start') {
-        bot.sendMessage(msg.from.id, startMessage, {
-            reply_markup: startOptions()
-        })
-    }
+  if (msg.text === '/start') {
+    bot.sendMessage(msg.from.id, startMessage, {
+      reply_markup: startOptions()
+    })
+  }
 })
 
 bot.on('callback_query', async (msg) => {
-    if (msg.data === 'new event') {
-        onNewEvent(bot, msg.message.chat.id, msg.message.message_id)
-    }
+  if (msg.data === 'new event') {
+    onNewEvent(bot, msg.message.chat.id, msg.message.message_id)
+  }
 
-    if (dateRegexp.test(msg.data)) {
-        onSelectTime(bot, msg.message.chat.id, msg.message.message_id, msg.data)
-    }
+  if (dateRegexp.test(msg.data)) {
+    onSelectTime(bot, msg.message.chat.id, msg.message.message_id, msg.data)
+  }
 
-    if (durationRegexp.test(msg.data)) {
-        const freeTime = await manager.showFreeTime(msg.data)
-        chooseTime(bot, msg.message.chat.id, msg.message.message_id, freeTime)
-    }
+  if (durationRegexp.test(msg.data)) {
+    const freeTime = await manager.showFreeTime(msg.data)
+    chooseTime(bot, msg.message.chat.id, msg.message.message_id, freeTime)
+  }
 
-    if (msg.data === 'home') {
-        goHome(bot, msg.message.chat.id, msg.message.message_id)
-    }
+  if (msg.data === 'home') {
+    goHome(bot, msg.message.chat.id, msg.message.message_id)
+  }
 
-    if (msg.data.startsWith('add')) {
-        await addBooking(
-            bot,
-            msg.message.chat.id,
-            msg.message.message_id,
-            msg.from.id,
-            msg.from.first_name,
-            msg.data,
-            manager
-        )
-    }
+  if (msg.data.startsWith('add')) {
+    await addBooking(
+      bot,
+      msg.message.chat.id,
+      msg.message.message_id,
+      msg.from.id,
+      msg.from.first_name,
+      msg.data,
+      manager
+    )
+  }
 
-    if (msg.data === 'my_bookings') {
-        await myBookings(
-            bot,
-            msg.message.chat.id,
-            msg.message.message_id,
-            msg.from.id,
-            manager
-        )
-    }
+  if (msg.data === 'my_bookings') {
+    await myBookings(
+      bot,
+      msg.message.chat.id,
+      msg.message.message_id,
+      msg.from.id,
+      manager
+    )
+  }
 
-    if (msg.data === 'cancel_booking') {
-        await cancelBooking(
-            bot,
-            msg.message.chat.id,
-            msg.message.message_id,
-            msg.from.id,
-            manager
-        )
-    }
+  if (msg.data === 'cancel_booking') {
+    await cancelBooking(
+      bot,
+      msg.message.chat.id,
+      msg.message.message_id,
+      msg.from.id,
+      manager
+    )
+  }
 
-    if (msg.data.startsWith('delete')) {
-        await deleteBooking(
-            bot,
-            msg.message.chat.id,
-            msg.message.message_id,
-            msg.from.id,
-            msg.data,
-            msg.from.first_name,
-            manager
-        )
-    }
+  if (msg.data.startsWith('delete')) {
+    await deleteBooking(
+      bot,
+      msg.message.chat.id,
+      msg.message.message_id,
+      msg.from.id,
+      msg.data,
+      msg.from.first_name,
+      manager
+    )
+  }
 
-    if (msg.data === "referal") {
-        // create referal link
-    }
+  if (msg.data === "referal") {
+    // create referal link
+  }
 })
--- src/lib/googleApi/calendar.js
+++ src/lib/googleApi/calendar.js
@@ -1,53 +1,53 @@
 const { google } = require('googleapis')
 
-const calendar = google.calendar({ version:'v3' })
+const calendar = google.calendar({ version: 'v3' })
 
-const { client_secret, client_id, redirect_uris, token, calendarId } = require('../../lib/config').googleCredentials
+const { client_secret, client_id, redirect_uris, token, calendarId } = require('../../config').googleCredentials
 
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
 
 oAuth2Client.setCredentials(token)
 
 class GoogleCalendar {
-    constructor() {
-        this.auth = oAuth2Client
-        this.calendarId = calendarId
-    }
+  constructor() {
+    this.auth = oAuth2Client
+    this.calendarId = calendarId
+  }
 
-    insertEvent(name, start, end) { 
-        return new Promise((resolve, reject) => {
-            calendar.events.insert({
-                auth: this.auth,
-                calendarId: this.calendarId,
-                resource: {
-                    summary: name,
-                    start: {
-                        dateTime: start,
-                        timeZone: 'Europe/Kiev'
-                    },
-                    end: {
-                        dateTime: end,
-                        timeZone: 'Europe/Kiev'
-                    },
-                }
-            }, (err, res) => {
-                if (err) reject(err)
-                resolve(res)
-            })
-        })     
-    }
-    deleteEvent(eventId) {
-        return new Promise((resolve, reject) => {
-            calendar.events.delete({
-                auth: this.auth,
-                calendarId: this.calendarId,
-                eventId: eventId
-            }, (err, res) => {
-                if (err) reject(err)
-                resolve(res)
-            })
-        })
-    }
+  insertEvent(name, start, end) {
+    return new Promise((resolve, reject) => {
+      calendar.events.insert({
+        auth: this.auth,
+        calendarId: this.calendarId,
+        resource: {
+          summary: name,
+          start: {
+            dateTime: start,
+            timeZone: 'Europe/Kiev'
+          },
+          end: {
+            dateTime: end,
+            timeZone: 'Europe/Kiev'
+          },
+        }
+      }, (err, res) => {
+        if (err) reject(err)
+        resolve(res)
+      })
+    })
+  }
+  deleteEvent(eventId) {
+    return new Promise((resolve, reject) => {
+      calendar.events.delete({
+        auth: this.auth,
+        calendarId: this.calendarId,
+        eventId: eventId
+      }, (err, res) => {
+        if (err) reject(err)
+        resolve(res)
+      })
+    })
+  }
 }
 
 module.exports = GoogleCalendar
